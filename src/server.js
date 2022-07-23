5dae566e1a507dac36f73836f2ca0d0da81f644f
Bobby Lee <ldg@dgit.co>
2022-07-24 00:54:09 +0900

    Setup Frontend

--- src/server.js
+++ src/server.js
@@ -2,4 +2,15 @@ import express from "express";
 
 const app = express();
 
-app.listen(3000);
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.render("home"));
+
+const PORT = 3000;
+
+const handleListening = () => console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
