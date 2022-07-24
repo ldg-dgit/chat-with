cd04a157931f33ea23a9b3bcb3ccc8cfadf2ca94
Bobby Lee <ldg@dgit.co>
2022-07-24 09:52:49 +0900

    Install ws

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -8,9 +10,14 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const PORT = 3000;
 
 const handleListening = () => console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);
 
-app.listen(PORT, handleListening);
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+server.listen(3000, handleListening);
